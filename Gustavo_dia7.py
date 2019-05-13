151c40df1e5c77c9de069fcc10237aa6a78f934d
GustavoaGH1S <gustavoabelflorentinperira11@gmail.com>
2019-05-13 18:47:32 +0200

    Hola

--- Gustavo_dia7.py
+++ Gustavo_dia7.py
@@ -1,3 +1,14 @@
+###############
+#  Comnado de GIT
+###############
+#git add
+#git, commit -m "el mensaje"
+#git push     (Para empujar el codigo de Local al Remoto)
+#git pull      (Para estirar el codigo fr Remoto al Local)
+##############
+#Comando para navegar en la Terminal
+##############
 
-#practica con control de verciones git
-   print("Hola que tal")
+# ls o dir    (Para ver archivos existentes)
+#cd nombres_de archivo   (Para entrar en el archivo)
+#cd ..         (Para salir de archivo)
